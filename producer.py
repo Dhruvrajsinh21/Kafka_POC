b8d8d0fe9a2155c58d363d0f69d10fc3b041f994
Dhruvrajsinh Solanki <115185535+Dhruvrajsinh21@users.noreply.github.com>
2025-02-03 19:17:32 +0530

    Update producer.py

--- producer.py
+++ producer.py
@@ -14,18 +14,40 @@ def delivery_report(err, msg):
     else:
         print(f"Message delivered to {msg.topic()} [{msg.partition()}]")
 
-def send_logs():
-    while True:
-        log_data = {
-            "timestamp": time.strftime("%Y-%m-%d %H:%M:%S"),
-            "level": random.choice(log_levels),
-            "message": "Sample log message",
-            "source": "KafkaProducer"
-        }
-        log_json = json.dumps(log_data)
-        producer.produce("logs", key="log", value=log_json, callback=delivery_report)
-        producer.flush()
-        print(f"Sent log: {log_data}")
-        time.sleep(2)
+# (Producer 1)
+def mysql_logs_producer():
+    log_data = {
+        "timestamp": time.strftime("%Y-%m-%d %H:%M:%S"),
+        "level": random.choice(log_levels),
+        "message": "MySQL query log",
+        "source": "MySQLProducer"
+    }
+    log_json = json.dumps(log_data)
+    producer.produce("logs", key="mysql-log", value=log_json, callback=delivery_report)
+    producer.flush()
+    print(f"Sent MySQL Log: {log_data}")
 
-send_logs()
+# (Producer 2)
+def nginx_logs_producer():
+    log_data = {
+        "timestamp": time.strftime("%Y-%m-%d %H:%M:%S"),
+        "level": random.choice(log_levels),
+        "message": "NGINX error log",
+        "source": "NGINXProducer"
+    }
+    log_json = json.dumps(log_data)
+    producer.produce("nginxlogs", key="nginx-log", value=log_json, callback=delivery_report)
+    producer.flush()
+    print(f"Sent NGINX Log: {log_data}")
+
+def start_random_producer():
+    while True:
+        selected_producer = random.choice([mysql_logs_producer, nginx_logs_producer])
+        selected_producer()
+        time.sleep(2) 
+
+if __name__ == "__main__":
+    try:
+        start_random_producer()
+    except KeyboardInterrupt:
+        print("Producer stopped.")
